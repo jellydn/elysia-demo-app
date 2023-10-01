aa88e23131bfd831337c1a7e06a569eeffb92d76
Huynh Duc Dung <dunghd.it@gmail.com>
2023-10-02 02:03:52 +0800

    feat: setup eslint for svelte

--- web/.eslintrc.cjs
+++ web/.eslintrc.cjs
@@ -5,6 +5,17 @@ module.exports = {
   ignorePatterns: ["dist", ".eslintrc.cjs", "vite.config.ts"],
   parserOptions: {
     project: ["./tsconfig.json", "./tsconfig.node.json"],
+    extraFileExtensions: [".svelte"],
   },
+  overrides: [
+    {
+      files: ["*.svelte"],
+      parser: "svelte-eslint-parser",
+      // Parse the `<script>` in `.svelte` as TypeScript by adding the following configuration.
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+      },
+    },
+  ],
   rules: {},
 };
