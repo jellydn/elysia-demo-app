c4d28640947215ea50fa085a2a6f4559d6efbed3
Huynh Duc Dung <dunghd.it@gmail.com>
2023-10-01 10:06:15 +0800

    feat: add logger

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { Elysia } from "elysia";
 
+import logger from "./logger";
+
 const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+logger.info(
+  `🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}`,
 );
