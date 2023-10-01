762c9a5466343af6ded322f4a619d97bb34ded5a
Huynh Duc Dung <dunghd.it@gmail.com>
2023-10-01 14:36:30 +0800

    feat: add end-to-end type safety

---
+++ src/client.ts
@@ -0,0 +1,21 @@
+import { edenTreaty } from "@elysiajs/eden";
+
+import type { App } from "./index";
+import logger from "./logger";
+
+const port = process.env.PORT ?? 3000;
+const app = edenTreaty<App>("http://0.0.0.0:" + port);
+
+const hello = await app.get();
+
+logger.info(hello.data); // Hello Elysia
+
+try {
+  const signIn = await app["sign-in"].post({
+    username: "admin",
+    password: "admin",
+  });
+  logger.info(signIn.data);
+} catch (error) {
+  logger.error(error);
+}
